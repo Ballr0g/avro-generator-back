1659b5b4aaad8fb00d264b714d996bf0887b1a92
Daniil Sagalov <danielsagel@outlook.com>
2023-03-12 00:53:28 +0300

    AVR-009 Updated tests to include minimal POST tests.

--- src/test/java/ru/hse/avrogen/component/ApicurioClientComponentTest.java
+++ src/test/java/ru/hse/avrogen/component/ApicurioClientComponentTest.java
@@ -2,21 +2,47 @@ package ru.hse.avrogen.component;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.vertx.core.json.JsonObject;
 import org.junit.jupiter.api.Test;
+import ru.hse.avrogen.dto.PostSchemaResponseDto;
 import ru.hse.avrogen.infrastructure.ApicurioRegistryTestResource;
 
+import javax.ws.rs.core.HttpHeaders;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 @QuarkusTestResource(value = ApicurioRegistryTestResource.class, restrictToAnnotatedClass = true)
 public class ApicurioClientComponentTest {
     @Test
-    public void testGetSchemasEndpoint() {
+    public void testGetSubjectsEndpoint() {
         given()
-                .when().get("/avroGenerator/v1/getSubjects")
+                .when()
+                .get("/avroGenerator/v1/getSubjects")
                 .then()
                 .statusCode(200)
-                .body(containsString("[]"));
+                .body(startsWith("["), endsWith("]"));
+    }
+
+    @Test
+    public void testPostNewSchemaEndpoint() {
+        JsonObject testInput = new JsonObject();
+        testInput.put("subjectName", "666");
+        testInput.put("schema", "{ \"type\": \"record\", \"name\": \"test\", \"fields\": [ { \"type\": \"string\", \"name\": \"field\" } ] }");
+
+        var postResult = given()
+                .when()
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(testInput.toString())
+                .post("/avroGenerator/v1/createSchema")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response()
+                .as(PostSchemaResponseDto.class);
+
+        assertEquals(1, postResult.id());
     }
 }
