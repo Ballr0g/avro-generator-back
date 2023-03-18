399f7137bab80379b96240b3a8a48e1bfd15d510
Daniil Sagalov <danielsagel@outlook.com>
2023-03-18 16:30:46 +0300

    AVR-014 Fixed AvroSchemaValidationStrategyImpl likely not compiling due to previous renames.

--- src/main/java/ru/hse/avrogen/util/schema/avro/AvroSchemaValidationStrategyImpl.java
+++ src/main/java/ru/hse/avrogen/util/schema/avro/AvroSchemaValidationStrategyImpl.java
@@ -2,18 +2,17 @@ package ru.hse.avrogen.util.schema.avro;
 
 import org.apache.avro.Schema;
 import org.jboss.resteasy.reactive.common.NotImplementedYet;
-import ru.hse.avrogen.dto.FieldRequirementsDto;
-import ru.hse.avrogen.dto.builders.FieldRequirementsBuilder;
+import ru.hse.avrogen.dto.builders.SchemaRequirementViolationDtoBuilder;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 
 @ApplicationScoped
 public class AvroSchemaValidationStrategyImpl implements AvroSchemaValidationStrategy {
-    private final FieldRequirementsBuilder fieldRequirementsBuilder = new FieldRequirementsBuilder();
+    private final SchemaRequirementViolationDtoBuilder schemaRequirementViolationDtoBuilder = new SchemaRequirementViolationDtoBuilder();
 
     // Todo: setup the nesting
-    private final FieldRequirementsDto rootFieldRequirement = fieldRequirementsBuilder.build();
+//    private final SchemaRequirementViolationDto rootFieldRequirement = fieldRequirementsBuilder.build();
 
 
     @Override
